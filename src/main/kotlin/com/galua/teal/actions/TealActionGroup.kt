43eeb1f31ab1f75d72ac03a1b1f653143a8bb6c2
Alexander Selyutin <cool.selutin99@yandex.ru>
2026-05-17 15:32:47 +0400

    Fix action group icon

--- src/main/kotlin/com/galua/teal/actions/TealActionGroup.kt
+++ src/main/kotlin/com/galua/teal/actions/TealActionGroup.kt
@@ -4,6 +4,7 @@
  */
 package com.galua.teal.actions
 
+import com.galua.teal.core.TealIcons
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DefaultActionGroup
 
@@ -11,12 +12,17 @@ import com.intellij.openapi.actionSystem.DefaultActionGroup
  * Shows the Teal action group only for selected Teal source files
  */
 class TealActionGroup : DefaultActionGroup() {
+    init {
+        templatePresentation.icon = TealIcons.FILE
+    }
+
     /**
      * Updates group visibility from the currently selected virtual file
      *
      * @param event action event used to update group presentation state
      */
     override fun update(event: AnActionEvent) {
+        event.presentation.icon = TealIcons.FILE
         event.presentation.isEnabledAndVisible = findSelectedTealFile(event) != null
     }
 }
